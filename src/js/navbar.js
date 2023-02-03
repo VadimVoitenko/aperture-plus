ecf3a9fee543cf7c762ee51b66bc485ee7743ce3
Vadim Voitenko <mirvoy8031@gmail.com>
2023-02-03 23:49:07 +0200

    Fix header & post sections

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,42 +1,43 @@
-// Мобильное меню бургер
+// Mobile menu burger
 function burgerMenu() {
-    const burger = document.querySelector('.burger')
-    const menu = document.querySelector('.menu')
-    const body = document.querySelector('body')
-    burger.addEventListener('click', () => {
-      if (!menu.classList.contains('active')) {
-        menu.classList.add('active')
-        burger.classList.add('active-burger')
-        body.classList.add('locked')
-      } else {
-        menu.classList.remove('active')
-        burger.classList.remove('active-burger')
-        body.classList.remove('locked')
-      }
-    })
-    // Вот тут мы ставим брейкпоинт навбара
-    window.addEventListener('resize', () => {
-      if (window.innerWidth > 991.98) {
-        menu.classList.remove('active')
-        burger.classList.remove('active-burger')
-        body.classList.remove('locked')
-      }
-    })
-  }
-  burgerMenu()
-  
-  
-  // Вызываем эту функцию, если нам нужно зафиксировать меню при скролле.
-  function fixedNav() {
-    const nav = document.querySelector('nav')
-  
-    // тут указываем в пикселях, сколько нужно проскроллить что бы наше меню стало фиксированным
-    const breakpoint = 1
-    if (window.scrollY >= breakpoint) {
-      nav.classList.add('fixed__nav')
+  const burger = document.querySelector('.burger')
+  const menu = document.querySelector('.menu')
+  const body = document.querySelector('body')
+
+  burger.addEventListener('click', () => {
+    if (!menu.classList.contains('active')) {
+      menu.classList.add('active')
+      burger.classList.add('active-burger')
+      body.classList.add('locked')
     } else {
-      nav.classList.remove('fixed__nav')
+      menu.classList.remove('active')
+      burger.classList.remove('active-burger')
+      body.classList.remove('locked')
     }
+  })
+
+  // navbar breakpoint
+  window.addEventListener('resize', () => {
+    if (window.innerWidth > 991.98) {
+      menu.classList.remove('active')
+      burger.classList.remove('active-burger')
+      body.classList.remove('locked')
+    }
+  })
+}
+
+burgerMenu()
+
+// Call this function, if we need to fix the menu when scrolling 
+function fixedNav() {
+  const nav = document.querySelector('nav')
+
+  // here it's indicated in pixels how much to scroll to make our menu fixed
+  const breakpoint = 1
+  if (window.scrollY >= breakpoint) {
+    nav.classList.add('fixed__nav')
+  } else {
+    nav.classList.remove('fixed__nav')
   }
-  window.addEventListener('scroll', fixedNav)
-  
+}
+window.addEventListener('scroll', fixedNav)
